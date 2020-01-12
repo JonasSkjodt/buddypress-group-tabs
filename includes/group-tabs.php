df4708ceca6d0649ad561d22af113ccd31778bd4
Jonas Skjodt <jonasmeister@gmail.com>
2020-01-12 21:41:11 +0100

    Update group-tabs.php

--- includes/group-tabs.php
+++ includes/group-tabs.php
@@ -3,8 +3,7 @@ if ( !defined( 'ABSPATH' ) ) exit;
 
 /**
  * Buddypress groups
- * Extra tabs for some groups 
- * 
+ * Extra tabs for buddypress groups 
  */
 
 if (bp_is_active( 'groups' ) ) :
@@ -14,18 +13,18 @@ class new_buddypress_group_tab extends BP_Group_Extension {
     // contruct the new group tab
     function __construct() {
     
-		// if the group is specific and not all the groups, add the if
-		if (bp_is_group('NAMEOFGROUP') && 'NAMEOFGROUP' == bp_current_item()  ) { 
+	// if the group is specific and not all the groups, add the if
+	if (bp_is_group('NAMEOFGROUP') && 'NAMEOFGROUP' == bp_current_item()  ) { 
     
         // add the new group tab specifics
         $args = array(
-            'slug'              => 'slug name', // url extension name of new group tab
-            'name'              => 'name', // name of new tab
-            'nav_item_position' => 25, // group submenu navigation position
-            'nav_item_name'     => 'name', // the navigation link name
-        );
-        parent::init( $args );
-		  }
+            	    'slug'              => 'slug name', // url extension name of new group tab
+            	    'name'              => 'name', // name of new tab
+            	    'nav_item_position' => 25, // group submenu navigation position
+            	    'nav_item_name'     => 'name', // the navigation link name
+        	);
+            parent::init( $args );
+	}
     }
     
     // show whats in the group tab
