f503484e2d36c5e37ae34bb855f152b19bb3d4a6
Jonas Skjodt <jonasmeister@gmail.com>
2020-01-12 21:43:30 +0100

    Update loader.php

--- loader.php
+++ loader.php
@@ -2,10 +2,10 @@
 /*
 Plugin Name: Buddypress Group Tabs
 Plugin URI: https://worklizard.com
-Description: This plugin ads group tabs with info to specific Worklizard groups
+Description: This plugin adds a code base for adding specific group tabs with info to specific Buddypress groups
 Version: 1.0
 Requires at least: WordPress 4.9 / BuddyPress 5.1
-Tested up to: WordPress 4.9 / BuddyPress 5.1
+Tested up to: WordPress 4.9 / BuddyPress 5.2
 License: GNU/GPL 2
 Author: Jonas Skjodt
 Author URI: https://jonaskjodt.com
@@ -14,8 +14,8 @@ Author URI: https://jonaskjodt.com
 // Exit if accessed directly
 if ( !defined( 'ABSPATH' ) ) exit;
  
-/* Only load code that needs BuddyPress to run once BP is loaded and initialized. */
-function load_worklizard_specific_group_tabs() {
+// Load our custom group tab code only after BP is loaded and initialized
+function load_buddypress_specific_group_tabs() {
     require( dirname( __FILE__ ) . '/includes/group-tabs.php' );
 }
-add_action( 'bp_include', 'load_worklizard_specific_group_tabs' );
+add_action( 'bp_include', 'load_buddypress_specific_group_tabs' );
